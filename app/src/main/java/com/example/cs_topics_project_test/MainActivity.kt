4943e78a746b344f45bdc90acf7ea3e2c59b6797
natalie-86 <nataliechung20070522@gmail.com>
2025-03-27 14:58:47 -0700

    button is working but not the text

--- app/src/main/java/com/example/cs_topics_project_test/MainActivity.kt
+++ app/src/main/java/com/example/cs_topics_project_test/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.cs_topics_project_test
 
 import android.content.Intent
+import android.os.Build.VERSION_CODES.R
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -25,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         }
         startActivity(Intent(this, SignInActivity::class.java))
     }
-    private val list :MutableList<String> = mutableListOf("What do you call a factory that makes okay products? ",
+    /*private val list: List<String> = listOf("What do you call a factory that makes okay products? ",
         "How do you organize a space party?",
         "Where do pirates buy their hooks?",
         "What did the janitor say when he jumped out of the closet?",
@@ -33,13 +34,19 @@ class MainActivity : AppCompatActivity() {
         "Why is so great about Switzerland?",
         "Why did the pony need a drink of water?",
         "What do you call a pig who knows how to use a knife?")
-    fun onClick(view: View){
+    fun onClick(view: android.view.View){
         val myRandomInt = Random.nextInt(list.size)
-        val t = findViewById<TextView>(R.id.textView6)
-        var b = findViewById<Button>(R.id.button)
-        t.text = list[myRandomInt].toString()
+
+        //val tv : TextView=findViewById(R.id.tv6)
+        //tv.setText(list[myRandomInt].toString())
+        val t: TextView = findViewById(R.id.tv6)
+        t.setText(list[myRandomInt].toString())
+        //var b: Button = findViewById<Button>(R.id.bt)
+        //t.text = list[myRandomInt].toString()
+
+
 
     }
-
+*/
 
 }
--- app/src/main/java/com/example/cs_topics_project_test/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/cs_topics_project_test/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.cs_topics_project_test.ui.home
 
+//import android.os.Build.VERSION_CODES.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +11,8 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.cs_topics_project_test.R
 import com.example.cs_topics_project_test.databinding.FragmentHomeBinding
 import kotlin.random.Random
+import android.widget.Button
+//import android.os.Build.VERSION_CODES.R
 
 class HomeFragment : Fragment() {
 
@@ -40,6 +43,25 @@ class HomeFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+    private val list: List<String> = listOf("What do you call a factory that makes okay products? ",
+        "How do you organize a space party?",
+        "Where do pirates buy their hooks?",
+        "What did the janitor say when he jumped out of the closet?",
+        "What did the Buffalo say to his little boy when he left the house?",
+        "Why is so great about Switzerland?",
+        "Why did the pony need a drink of water?",
+        "What do you call a pig who knows how to use a knife?")
+    //fun onClick(view: android.view.View){}
+
+    fun onClick(view: View) {
+        val myRandomInt = Random.nextInt(list.size)
+        //val tv : TextView=findViewById(R.id.tv6)
+        val t: TextView = view.findViewById(R.id.tv6)
+        //t.text = list[myRandomInt].toString()
+        t.text = list[myRandomInt]
+        //var b: Button = findViewById<Button>(R.id.bt)
+        //t.text = list[myRandomInt].toString()
     }
 
 
